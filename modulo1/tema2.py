0706afd37dfb49437614798c009c3fd7da6e4ca7
Victorfernandolopez <victorfernandolopez1988@gmail.com>
2025-03-17 18:50:36 -0300

    modulo1 - tema2

---
+++ modulo1/tema2.py
@@ -0,0 +1,12 @@
+"""
+Mini proyecto 2: Preparación del entorno
+Objetivo: Asegurarte de que tu entorno de trabajo esté correctamente configurado.
+
+Instrucciones:
+
+Instala Python (si no lo tienes ya instalado).
+Instala VS Code y la extensión de Python.
+Crea un nuevo archivo Python (.py) y escribe un programa simple que imprima "Preparación del entorno completada". Ejecuta el programa desde VS Code.
+"""
+
+print("Preparación del entorno completada")
